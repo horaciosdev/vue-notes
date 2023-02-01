b1992a88c4b6e544c662bca75aa2fb0ac9d43e1b
Horacio <horacio.schumann@gmail.com>
2023-02-01 16:50:40 -0300

    Initial commit, vue-notes base project ready.

---
+++ src/App.vue
@@ -0,0 +1,182 @@
+<script setup>
+import { ref } from "vue";
+
+const showModal = ref(false);
+const newNote = ref("");
+const errorMessage = ref("");
+const notes = ref([]);
+const noteMaxLenght = 190;
+
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
+
+const addNote = () => {
+  if (newNote.value.length < 10) {
+    return (errorMessage.value = "Note needs to be 10 characteres or more.");
+  }
+  if (newNote.value.length > noteMaxLenght) {
+    return (errorMessage.value = "Max 190 characteres.");
+  }
+
+  notes.value.push({
+    id: Math.floor(Math.random() * 9000000),
+    text: newNote.value,
+    date: new Date(),
+    backgroundColor: getRandomColor(),
+  });
+  showModal.value = false;
+  newNote.value = "";
+  errorMessage.value = "";
+};
+</script>
+
+<template>
+  <main>
+    <div v-if="showModal" class="overlay">
+      <div class="modal">
+        <textarea
+          v-model.trim="newNote"
+          name="note"
+          id="note"
+          cols="30"
+          rows="10"
+        >
+        </textarea>
+        <span
+          class="note-lenght"
+          :style="[newNote.length > noteMaxLenght ? { color: 'red' } : '']"
+          >{{ newNote.length }}/{{ noteMaxLenght }}</span
+        >
+        <p v-if="errorMessage">{{ errorMessage }}</p>
+        <button @click="addNote()">Add Note</button>
+        <button class="cancel" @click="showModal = false">Canvel</button>
+      </div>
+    </div>
+
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button @click="showModal = true">+</button>
+      </header>
+      <div class="cards-container">
+        <div
+          v-for="note in notes"
+          :key="note.id"
+          class="card"
+          :style="{ backgroundColor: note.backgroundColor }"
+        >
+          <p class="main-text">
+            {{ note.text }}
+          </p>
+          <p class="date">{{ note.date.toLocaleDateString("pt-BR") }}</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
+
+header button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(121, 120, 120);
+  border-radius: 50%;
+  color: #fff;
+  font-size: 20px;
+}
+
+.card {
+  color: #222;
+  width: 225px;
+  height: 225px;
+  background-color: rgb(237, 182, 44);
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  font-weight: bold;
+  word-break: break-word;
+}
+.date {
+  font-size: 12.5px;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  width: 750px;
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+}
+
+.modal .cancel {
+  background-color: red;
+  margin-top: 7px;
+}
+
+.modal p {
+  color: red;
+}
+
+.modal .note-lenght {
+  color: #222;
+  font-style: italic;
+}
+</style>
